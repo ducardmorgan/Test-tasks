d9a613e35a5764acdfa69cd43094824d0c55b233
Ducard Morgan <ducardmorgan@gmail.com>
2023-02-13 17:35:27 +0600

    Исправил

--- Chips/Chip.cs
+++ Chips/Chip.cs
@@ -10,27 +10,20 @@ namespace Chips
     {
         public Chip() { }
 
-        private int[]? p_arrayChips;
-        private int steps, count;
+        private int[]? p_arrayChips;// массив с которым будет работать класс
+        private int steps, count;//счетсчик шагов и среднее арифметическое массива
         public int[] arrayChips {
             get { return p_arrayChips; }
             set { 
-                p_arrayChips = value;
-                steps = count = 0;
+                p_arrayChips = value; //присваем значение массива
+                steps = count = 0;// чистка переменных информации от старого массива
                 for (int i = 0; i < p_arrayChips.Length; i++)
                     count+= p_arrayChips[i];
-                count /= p_arrayChips.Length;
+                count /= p_arrayChips.Length;// получение сред. арифмет. нового массива
             }
         }
 
-        bool CheckCloseLeft(int pos)
-        {
-            if (p_arrayChips[pos] < count)
-                return true;
-            return false;
-        }
-
-        bool CheckCloseRight(int pos)
+        bool CheckClose(int pos)//проверка ближнего элемента массива
         {
             if (p_arrayChips[pos] < count)
                 return true;
@@ -38,45 +31,55 @@ namespace Chips
         }
 
 
-        void Move(int pos_A, int pos_B)
+        void Move(int pos_A, int pos_B)//передвижение фишек и ведение счёта шагов
         {
-            int stepsIn = p_arrayChips[pos_A] - count;
-            p_arrayChips[pos_A]-=stepsIn;
-            p_arrayChips[pos_B]+=stepsIn;
-            steps+=stepsIn;
+            p_arrayChips[pos_A]--;
+            p_arrayChips[pos_B]++;
+            steps++;
         }
 
         
 
         public string GetResult()
         {
-            while (p_arrayChips.Max() != count)
+            while (p_arrayChips.Max() != count)//пока максимальный элемент не будет равен среднему занчению
             {
-                int i = p_arrayChips.ToList().IndexOf(p_arrayChips.Max());
-                int indexOfLeft = 0, indexOfRight = 0;
-                if (i == 0)
+                int i = p_arrayChips.ToList().IndexOf(p_arrayChips.Max());//индекс максимального элемента
+                int j = p_arrayChips.ToList().IndexOf(p_arrayChips.Min());//индекс минимального элемента
+                int len = p_arrayChips.Length;//длина массива
+                int indexOfLeft = 0, indexOfRight = 0;//индексы ближних элементов 
+                if (i == 0)//присвоение индексов ближних элементов,если максимальный элемент первый
                 {
                     indexOfRight = 1;
-                    indexOfLeft = p_arrayChips.Length - 1;
+                    indexOfLeft = len - 1;
                 }
-                else if (i == p_arrayChips.Length - 1)
+                else if (i == len - 1)//присвоение индексов ближних элементов,если максимальный элемент последний
                 {
                     indexOfLeft = i - 1;
                     indexOfRight = 0;
                 }
-                else
+                else//присвоение индексов ближних элементов,если максимальный элемент не на границе массива
                 {
                     indexOfRight = i + 1;
                     indexOfLeft = i - 1;
                 }
-                while (p_arrayChips[i] > count)
+                while (p_arrayChips[i] > count)//работа с маскимальным элементом, пока не будет равен ср.зн.
                 {
-                    if(CheckCloseLeft(indexOfLeft)) Move(i, indexOfLeft);
-                    else if (CheckCloseRight(indexOfRight)) Move(i,indexOfRight);
-                    else Move(i, indexOfLeft);
+                    if(CheckClose(indexOfLeft)) Move(i, indexOfLeft);//проверка элемента слева
+                    else if (CheckClose(indexOfRight)) Move(i,indexOfRight);//проверка элемента справа
+                    else if(i > j)
+                    {
+                        if ((i + 1) - (j + 1) > p_arrayChips.Length - (i + 1) + j) Move(i, indexOfRight);//если до минимального путь короче через элемент справа
+                        else Move(i, indexOfLeft);//если до минимального путь короче через элемент слева
+                    }
+                    else
+                    {
+                        if ((j + 1) - (i + 1) > p_arrayChips.Length - (j + 1) + i) Move(i, indexOfLeft);//если до минимального путь короче через элемент слева
+                        else Move(i, indexOfRight);//если до минимального путь короче через элемент справа
+                    }
                 }
             }
-            return $"Steps ={steps}";
+            return $"Steps ={steps}";//вывод кол-во шагов для решения
         }
     }
 }
--- Chips/Program.cs
+++ Chips/Program.cs
@@ -1,8 +1,9 @@
 ﻿using Chips;
 Chip chip = new Chip();
-int[] Test1 = { 6, 2, 4, 10, 3 };
+int[] Test1 = { 1, 5, 9, 10, 5 }; 
 int[] Test2 = { 1, 2, 3 };
 int[] Test3 = { 0, 1, 1, 1, 1, 1, 1, 1, 1, 2 };
+int[] Test4 = { 6, 2, 4, 10, 3 };
 
 
 Console.WriteLine("Test 1:");
@@ -13,5 +14,8 @@ chip.arrayChips = Test2;
 Console.WriteLine(chip.GetResult());
 Console.WriteLine("Test 3:");
 chip.arrayChips = Test3;
+Console.WriteLine(chip.GetResult());
+Console.WriteLine("Test 4:");
+chip.arrayChips = Test4;
 Console.WriteLine(chip.GetResult());
 
