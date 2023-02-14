cef02d968f5d12b9c7984491114375845513b319
Ducard Morgan <ducardmorgan@gmail.com>
2023-02-14 23:14:39 +0600

    Исправил

--- Chips/Chip.cs
+++ Chips/Chip.cs
@@ -11,75 +11,146 @@ namespace Chips
         public Chip() { }
 
         private int[]? p_arrayChips;// массив с которым будет работать класс
-        private int steps, count;//счетсчик шагов и среднее арифметическое массива
-        public int[] arrayChips {
+        private char[]? p_regChips;
+        private int p_steps, p_count;//счетсчик шагов и среднее арифметическое массива
+        public int[] ArrayChips {
             get { return p_arrayChips; }
             set { 
                 p_arrayChips = value; //присваем значение массива
-                steps = count = 0;// чистка переменных информации от старого массива
+                p_regChips = new char[value.Length];
+                p_steps = p_count = 0;// чистка переменных информации от старого массива
                 for (int i = 0; i < p_arrayChips.Length; i++)
-                    count+= p_arrayChips[i];
-                count /= p_arrayChips.Length;// получение сред. арифмет. нового массива
+                {
+                    p_regChips[i] = 'X';
+                    p_count += p_arrayChips[i];
+                }
+                p_count /= p_arrayChips.Length;// получение сред. арифмет. нового массива
             }
         }
 
+        public int Steps
+        {
+            get { return p_steps; }
+        }
         bool CheckClose(int pos)//проверка ближнего элемента массива
         {
-            if (p_arrayChips[pos] < count)
+            if (p_arrayChips[pos] < p_count)
                 return true;
             return false;
         }
 
 
-        void Move(int pos_A, int pos_B)//передвижение фишек и ведение счёта шагов
+        void Move(int pos_A, int pos_B, int step)//передвижение фишек и ведение счёта шагов
         {
-            p_arrayChips[pos_A]--;
-            p_arrayChips[pos_B]++;
-            steps++;
+            p_arrayChips[pos_A]-=step;
+            p_arrayChips[pos_B]+=step;
+            p_steps += step;
         }
 
-        
-
-        public string GetResult()
+        void HardMove(int indexOfMax, int indexOfMin, int stepsLeftSide, int stepsRightSide , int indexOfRight, int indexOfLeft, int length)
         {
-            while (p_arrayChips.Max() != count)//пока максимальный элемент не будет равен среднему занчению
+            int i;
+            int step = 0;
+            if (stepsLeftSide > stepsRightSide && p_regChips[indexOfRight] != 'L')//если до минимального путь короче через элемент справа
             {
-                int i = p_arrayChips.ToList().IndexOf(p_arrayChips.Max());//индекс максимального элемента
-                int j = p_arrayChips.ToList().IndexOf(p_arrayChips.Min());//индекс минимального элемента
+                i = indexOfRight;
+                while (i != indexOfMin)
+                {
+                    step += p_arrayChips[i];
+                    if (i == length - 1) i = 0;
+                    else i++;
+                }
+                step += p_arrayChips[indexOfMin];
+                step = stepsRightSide * p_count - step;
+                if (p_arrayChips[indexOfMax] - p_count < step) step = p_arrayChips[indexOfMax] - p_count;
+                p_regChips[indexOfMax] = 'R';
+                Move(indexOfMax, indexOfRight, step);
+            }
+            else if (p_regChips[indexOfLeft] != 'R')//если до минимального путь короче через элемент слева
+            {
+                i = indexOfLeft;
+                while (i != indexOfMin)
+                {
+                    step += p_arrayChips[i];
+                    if (i == 0) i = length - 1;
+                    else i--;
+                }
+                step += p_arrayChips[indexOfMin];
+                step = stepsLeftSide * p_count - step;
+                if (p_arrayChips[indexOfMax] - p_count < step) step = p_arrayChips[indexOfMax] - p_count;
+                p_regChips[indexOfMax] = 'L';
+                Move(indexOfMax, indexOfLeft, step);
+            }
+        }
+
+        public void СountTheSteps()
+        {
+            while (p_arrayChips.Max() != p_count)//пока максимальный элемент не будет равен среднему занчению
+            {
+                int step, stepsLeftSide, stepsRightSide;
+                int indexOfMax = p_arrayChips.ToList().IndexOf(p_arrayChips.Max());//индекс максимального элемента
+                int indexOfMin = p_arrayChips.ToList().IndexOf(p_arrayChips.Min());//индекс минимального элемента
                 int len = p_arrayChips.Length;//длина массива
                 int indexOfLeft = 0, indexOfRight = 0;//индексы ближних элементов 
-                if (i == 0)//присвоение индексов ближних элементов,если максимальный элемент первый
+                if (indexOfMax == 0)//присвоение индексов ближних элементов,если максимальный элемент первый
                 {
                     indexOfRight = 1;
                     indexOfLeft = len - 1;
                 }
-                else if (i == len - 1)//присвоение индексов ближних элементов,если максимальный элемент последний
+                else if (indexOfMax == len - 1)//присвоение индексов ближних элементов,если максимальный элемент последний
                 {
-                    indexOfLeft = i - 1;
+                    indexOfLeft = indexOfMax - 1;
                     indexOfRight = 0;
                 }
                 else//присвоение индексов ближних элементов,если максимальный элемент не на границе массива
                 {
-                    indexOfRight = i + 1;
-                    indexOfLeft = i - 1;
+                    indexOfRight = indexOfMax + 1;
+                    indexOfLeft = indexOfMax - 1;
                 }
-                while (p_arrayChips[i] > count)//работа с маскимальным элементом, пока не будет равен ср.зн.
+                while (p_arrayChips[indexOfMax] > p_count)//работа с маскимальным элементом, пока не будет равен ср.зн.
                 {
-                    if(CheckClose(indexOfLeft)) Move(i, indexOfLeft);//проверка элемента слева
-                    else if (CheckClose(indexOfRight)) Move(i,indexOfRight);//проверка элемента справа
-                    else if(i > j)
+                    step = 1;
+                    if (CheckClose(indexOfLeft) && CheckClose(indexOfRight))
                     {
-                        if ((i + 1) - (j + 1) > p_arrayChips.Length - (i + 1) + j) Move(i, indexOfRight);//если до минимального путь короче через элемент справа
-                        else Move(i, indexOfLeft);//если до минимального путь короче через элемент слева
+                        if (p_count - p_arrayChips[indexOfLeft] <= p_count - p_arrayChips[indexOfRight])
+                        {
+                            Move(indexOfMax, indexOfLeft, step);
+                            p_regChips[indexOfMax] = 'L';
+                        }
+                        else
+                        {
+                            Move(indexOfMax, indexOfRight, step);
+                            p_regChips[indexOfMax] = 'R';
+                        }
+                    }
+                    else if (CheckClose(indexOfLeft))
+                    {
+                        Move(indexOfMax, indexOfLeft, step);
+                        p_regChips[indexOfMax] = 'L';
+                    }
+                    else if (CheckClose(indexOfRight))
+                    {
+                        Move(indexOfMax, indexOfRight, step);
+                        p_regChips[indexOfMax] = 'R';
                     }
                     else
                     {
-                        if ((j + 1) - (i + 1) > p_arrayChips.Length - (j + 1) + i) Move(i, indexOfLeft);//если до минимального путь короче через элемент слева
-                        else Move(i, indexOfRight);//если до минимального путь короче через элемент справа
+                        if (indexOfMax > indexOfMin)
+                        {
+                            stepsLeftSide = (indexOfMax + 1) - (indexOfMin + 1);//путь слева
+                            stepsRightSide = p_arrayChips.Length - (indexOfMax + 1) + indexOfMin + 1;//путь справа
+                            HardMove(indexOfMax, indexOfMin, stepsLeftSide, stepsRightSide, indexOfRight, indexOfLeft, len);
+                        }
+                        else
+                        {
+                            stepsLeftSide = p_arrayChips.Length - (indexOfMin + 1) + indexOfMax + 1;//путь слева
+                            stepsRightSide = (indexOfMin + 1) - (indexOfMax + 1);//путь справа
+                            HardMove(indexOfMax, indexOfMin, stepsLeftSide, stepsRightSide, indexOfRight, indexOfLeft, len);
+                        }
+                        break;
                     }
                 }
             }
-            return $"Steps ={steps}";//вывод кол-во шагов для решения
         }
     }
 }
--- Chips/Program.cs
+++ Chips/Program.cs
@@ -1,21 +1,37 @@
 ﻿using Chips;
 Chip chip = new Chip();
-int[] Test1 = { 1, 5, 9, 10, 5 }; 
-int[] Test2 = { 1, 2, 3 };
-int[] Test3 = { 0, 1, 1, 1, 1, 1, 1, 1, 1, 2 };
-int[] Test4 = { 6, 2, 4, 10, 3 };
+
+//Данные для ввода
+// 1, 5, 9, 10, 5
+// 1, 2, 3 
+// 0, 1, 1, 1, 1, 1, 1, 1, 1, 2 
+// 6, 2, 4, 10, 3
+// 0, 10, 0, 8, 3, 10, 7, 0, 9, 3 
 
 
-Console.WriteLine("Test 1:");
-chip.arrayChips = Test1;
-Console.WriteLine(chip.GetResult());
-Console.WriteLine("Test 2:");
-chip.arrayChips = Test2;
-Console.WriteLine(chip.GetResult());
-Console.WriteLine("Test 3:");
-chip.arrayChips = Test3;
-Console.WriteLine(chip.GetResult());
-Console.WriteLine("Test 4:");
-chip.arrayChips = Test4;
-Console.WriteLine(chip.GetResult());
+int numTest = 1;
+again:
+try
+{
+    Console.WriteLine($"\nТест №{numTest}");
+    Console.WriteLine("Введите массив чисел:");
+
+    string[] userString = Console.ReadLine().Replace(" ", "").Split(",");
+    int[] array = new int[userString.Length];
+    for (int i = 0; i < array.Length; i++)
+        array[i] = int.Parse(userString[i]);
+
+    chip.ArrayChips = array;
+    chip.СountTheSteps();
+    Console.WriteLine($"Шагов для решения:{chip.Steps}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+numTest++;
+goto again;
+
+
+
 
